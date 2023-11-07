771ed794abc526fd4d377d117f0b2fb40ab9d56c
Dio Brando <wiut00009027@gmail.com>
2023-11-07 22:13:59 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://ec2-13-49-21-196.eu-north-1.compute.amazonaws.com")  // URL of the frontend
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
